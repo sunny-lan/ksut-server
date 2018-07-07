a1dc8ac7f7af803e1ae0c936e86c890cc23559e9
Sunny <sunny.lan.coder@gmail.com>
2018-07-07 13:15:49 -0400

    fix missing require issue

--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
 const bluebird = require('bluebird');
 const redis = require('redis');
 const { isHeroku } = require('../config/dev');
-const redisConf = require('./redis-config');
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
@@ -9,7 +8,7 @@ function create() {
     if (isHeroku())
         return redis.createClient(process.env.REDIS_URL);
     else
-        return redis.createClient(redisConf);
+        return redis.createClient(require('./redis-config'));
 }
 
 const db = create();
