6b6f007b4f08e62c742cb91d32544c579b7b461c
Sunny <sunny.lan.coder@gmail.com>
2018-07-19 19:46:20 -0400

    added support for react router

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const expressWs = require('express-ws');
 const { defaultPort } = require('./config');
 const handleClient = require('./client/websocket');
 const app = express();
+
 expressWs(app);
 
 app.use(express.static('build'));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+    res.sendFile('build/index.html');
+});
 
 app.ws('/', function (ws) {
     handleClient(ws);
