0a519b002edea30d0a368aefe948579d0aec44ba
Sunny <sunny.lan.coder@gmail.com>
2018-07-24 17:36:55 -0400

    fixed missing file

---
+++ command/script/index.js
@@ -0,0 +1,86 @@
+const uuid = require('uuid/v4');
+const {db} = require('../../db');
+const ClientScriptManager = require('./client');
+const ServerScriptManager = require('./server');
+const ScriptInfoManager = require('./info');
+const tables = require('./tables');
+
+const infoRegex = /\/\/ksut: info begin([\s\S]*)\/\/ksut: info end/;
+const serverRegex = /\/\/ksut: server code begin([\s\S]*)\/\/ksut: server code end/;
+const clientRegex = /\/\/ksut: client code begin([\s\S]*)\/\/ksut: client code end/;
+
+class ScriptManager {
+    constructor(client) {
+        this._c = client;
+        this._server = new ServerScriptManager(client);
+    }
+
+    //NOTE: scriptID should be generated on first time by client
+    async compile(scriptID, newCode) {
+        //if there is new code, overwrite, otherwise retrieve from db
+        let tasks = [];
+        if (!newCode)
+            newCode = await this._c.s('redis:hget', tables.code, scriptID);
+        else
+            tasks.push(this._c.s('redis:hset', tables.code, scriptID, newCode));
+
+        const scriptInfo = newCode.match(infoRegex),
+            serverCode = newCode.match(serverRegex),
+            clientCode = newCode.match(clientRegex);
+
+        if (!serverCode && !clientCode)
+            throw new Error('No code sent. Code must be flagged with "//ksut: ..."');
+
+        await ScriptInfoManager.remove(scriptID);
+
+        if (scriptInfo)
+            tasks.push(ScriptInfoManager.save(scriptID, scriptInfo[1]));
+
+        if (clientCode)
+            tasks.push(ClientScriptManager.compile(scriptID, clientCode[1]));
+
+        if (serverCode)
+            tasks.push(ServerScriptManager.save(scriptID, serverCode[1]));
+
+        await Promise.all(tasks);
+    }
+
+    async instantiate(scriptID) {
+        const instanceID = uuid();
+        await Promise.all([
+            this._c.s('redis:hset', tables.instances, instanceID, scriptID),
+            this._server.instantiate(instanceID, scriptID),
+        ]);
+        return instanceID;
+    }
+
+    async destroyInstance(instanceID) {
+        await Promise.all([
+            db.sremAsync(tables.unstarted, instanceID),
+            this._c.s('redis:hdel', tables.instances, instanceID),
+            this._c.s('redis:publish', instanceID, 'stop'),
+        ]);
+    }
+
+    //client references
+
+    static async fetch(...args) {
+        return ClientScriptManager.fetch(...args);
+    }
+
+    //info references
+
+    static async fetchInfo(...args) {
+        return ScriptInfoManager.fetch(...args);
+    }
+
+    static async search(...args) {
+        return ScriptInfoManager.search(...args);
+    }
+
+    //server references
+    static async _init(...args) {
+        return ServerScriptManager.init(...args);
+    }
+}
+module.exports = ScriptManager;
